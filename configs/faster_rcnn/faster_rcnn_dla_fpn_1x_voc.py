67d0f05d2e7f434abae5c60f2494f854369988d6
goodxue <627807499@qq.com>
2022-05-26 22:11:43 +0800

    train

--- configs/faster_rcnn/faster_rcnn_dla_fpn_1x_voc.py
+++ configs/faster_rcnn/faster_rcnn_dla_fpn_1x_voc.py
@@ -3,3 +3,19 @@ _base_ = [
     '../_base_/datasets/voc07.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
+
+resume_from = './tutorial_exps/latest.pth'  # Resume checkpoints from a given path, the training will be resumed from the epoch when the checkpoint's is saved.
+dataset_type = 'VOCDataset'
+work_dir = './tutorial_exps'
+device = 'cuda'
+evaluation = dict(  # The config to build the evaluation hook, refer to https://github.com/open-mmlab/mmdetection/blob/master/mmdet/core/evaluation/eval_hooks.py#L7 for more details.
+    interval=1)
+
+optimizer = dict(type='SGD', lr=0.0005, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+# actual epoch = 3 * 3 = 9
+lr_config = dict(policy='step', step=[3])
+# runtime settings
+runner = dict(
+    type='EpochBasedRunner', max_epochs=4)  # actual epoch = 4 * 3 = 12
